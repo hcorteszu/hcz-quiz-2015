7b8cad9d4f237ed525f07d1cb8ff949d66b3e9d8
Heriberto Cortés <hcorteszu@gmail.com>
2015-06-24 16:46:58 -0300

    hcz Aplicación express-partials

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var quizControler = require('../controllers/quiz_controller');
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
