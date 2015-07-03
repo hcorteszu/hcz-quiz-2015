29f58399229eccde9f9d3ffeef4aa394a520e6f0
Heriberto Cortés <hcorteszu@gmail.com>
2015-07-03 13:19:52 -0300

    hcz versión con rama créditos incluida

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,4 +10,8 @@ exports.answer = function(req, res) {
 	} else {
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
+
+};
+exports.author = function(req,res) {
+	res.render('quizes/author');
 };
