54cc5031d51519cf224c0900874bef8b899e91ee
Heriberto Cortés <hcorteszu@gmail.com>
2015-07-10 22:32:09 -0300

    hcz versión con Postgres y múltiples preguntas

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload : quizId
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
-router.get('/quizes/author',   quizController.author);
+router.get('/quizes',                        quizController.index);
+router.get('/quizes/:quizId(\\d+)',          quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',   quizController.answer);
+router.get('/quizes/author',                 quizController.author);
 module.exports = router;
